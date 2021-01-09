7efe55a10579cad0b4f22fc859ffc29a263b5041
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2021-01-09 19:05:36 +0300

    Updates

---
+++ precache-manifest.8f46111fd349fa7bd51ff8ff42165919.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a20ff0d8cd50ea00c84c66018a5a191",
+    "url": "/questionnaire/index.html"
+  },
+  {
+    "revision": "eb59ee618f51f45e4004",
+    "url": "/questionnaire/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "8988e5b3a727aafe6db8",
+    "url": "/questionnaire/static/css/main.abd79676.chunk.css"
+  },
+  {
+    "revision": "eb59ee618f51f45e4004",
+    "url": "/questionnaire/static/js/2.b68809bc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/questionnaire/static/js/2.b68809bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8988e5b3a727aafe6db8",
+    "url": "/questionnaire/static/js/main.c0bccb94.chunk.js"
+  },
+  {
+    "revision": "823d4fb9c9360cb8893e",
+    "url": "/questionnaire/static/js/runtime-main.7a8d77a9.js"
+  },
+  {
+    "revision": "3442dc4f132587d89f99e2a1d0f7e061",
+    "url": "/questionnaire/static/media/how_gay_are_you.3442dc4f.png"
+  },
+  {
+    "revision": "5376b2d3565f5d45ef24e9c9d2138628",
+    "url": "/questionnaire/static/media/img_test.5376b2d3.png"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/questionnaire/static/media/persik.4e1ec840.png"
+  }
+]);
