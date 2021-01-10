f78dcc4970ce897eea893b90ac736701feb3bf76
John Doe <johndoe@example.com>
2021-01-11 01:55:19 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/questionnaire/precache-manifest.20c47ec2a6df73f966951bc4742511c6.js"
+  "/questionnaire/precache-manifest.21eee04e0a22f557686c50c4ffc753e7.js"
 );
 
 self.addEventListener('message', (event) => {
