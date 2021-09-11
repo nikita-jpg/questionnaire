421b7b8c87ff706fb199b9dd4f425328f4607352
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2021-09-11 18:32:45 +0300

    added Tooltip from material ui

--- src/components/CustomTooltip/CustomTooltip.js
+++ src/components/CustomTooltip/CustomTooltip.js
@@ -1,30 +1,55 @@
 import React, { useCallback, useState } from 'react';
-import { Tooltip } from '@vkontakte/vkui';
 import "./CustomTooltip.css";
+import { ClickAwayListener, Tooltip } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+    tooltip: {
+        backgroundColor: 'var(--main-therd-bg-color)',
+        color: 'var(--text_secondary)',
+        maxWidth: 220,
+        fontSize: "14px",
+        border: "1px solid var(--main-therd-bg-color)"
+    },
+    arrow: {
+        color: 'var(--main-therd-bg-color)',
+        '&::before': {
+            border: "1px solid var(--main-therd-bg-color)"
+        }
+    }
+}));
 
 const CustomTooltip = ({children, text, defaultIsShown=false}) => {
+    const classes = useStyles();
+
     const [isVisible, setIsVisible] = useState(defaultIsShown);
 
     const open = useCallback(() => {
+        console.log("open")
         setIsVisible(true);
     });
 
     const close = useCallback(() => {
+        console.log("close")
         setIsVisible(false);
     });
 
     return (
-        <Tooltip
-            text={text}
-            isShown={isVisible}
-            onClose={close}
-            cornerOffset={50}
-            offsetX={50}
-        >
-            <span onClick={open}>
-                {children}
-            </span>
-        </Tooltip>
+        <ClickAwayListener onClickAway={close}>
+            <Tooltip
+                title={text}
+                disableFocusListener
+                disableHoverListener
+                arrow
+                onClose={close}
+                open={isVisible}
+                classes={{ tooltip: classes.tooltip, arrow: classes.arrow }}
+            >
+                <span onClick={open}>
+                    {children}
+                </span>
+            </Tooltip>
+        </ClickAwayListener>
     )
 }
 
