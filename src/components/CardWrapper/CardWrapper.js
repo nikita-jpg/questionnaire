1eec74a98f2ca46cafd8fef73281316779dd4185
Nikita <nicitazaletin@yandex.ru>
2021-10-02 22:19:49 +0300

    Добавил тестовую гифку

--- src/components/CardWrapper/CardWrapper.js
+++ src/components/CardWrapper/CardWrapper.js
@@ -1,29 +1,29 @@
 import { ContentCard } from "@vkontakte/vkui"
 import React, { useState } from 'react';
 import './CardWrapper.css';
-import defaultImage from '../../img/defaultImage.jpg'
+import defaultImage from '../../img/defaultImage.svg'
 
 const CardWrapper = ({title,percentProgress,numberOfQuestions,imageName,description,cardClick=()=>{}, downloadImage=()=>{}}) => {
 
     //Устанавливаем дефолтую картинку на карточки
-    const [image, setImage] = useState(defaultImage)
+    const [image, setImage] = useState(<div className="defaultCSS"></div>)
 
     //Загружаем основную картинку
-    downloadImage(imageName).then(imageData=>{setImage(`data:image/jpeg;base64,${imageData}`)})
+    // downloadImage(imageName).then(imageData=>{setImage(`data:image/jpeg;base64,${imageData}`)})
 
     return(
-        <ContentCard
-            header={
-                <div className="CardWrapper__title">
-                    <div>{title}</div>
-                    <div>{percentProgress}/{numberOfQuestions}</div>
-                </div>
-            }
-            mode={"tint"}
-            onClick={cardClick}
-            image={image}
-            caption={description}
-            className="CardWrapper__Card"
+    <ContentCard
+        header={
+            <div className="CardWrapper__title">
+                <div>{title}</div>
+                <div>{percentProgress}/{numberOfQuestions}</div>
+            </div>
+        }
+        mode={"tint"}
+        onClick={cardClick}
+        image={defaultImage}
+        caption={description}
+        className="CardWrapper__Card"
     />
 )}
 
